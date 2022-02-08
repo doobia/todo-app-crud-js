183b591a26c30646373afcb9a9d0d989dcb2f959
doobia <doobiatief@yahoo.com>
2022-02-08 11:54:14 -0500

    localstorage added

--- main.js
+++ main.js
@@ -28,35 +28,46 @@ let formValidation = () => {
   }
 };
 
-let data = {};
+let data = [];
 
 let acceptData = () => {
-  data["text"] = textInput.value;
-  data["date"] = dateInput.value;
-  data["description"] = textarea.value;
-
+  data.push({
+    text: textInput.value,
+    date: dateInput.value,
+    description: textarea.value,
+  });
+  console.log(data);
+  localStorage.setItem("data", JSON.stringify(data));
+  // localStorage.getItem()
   createTasks();
 };
 
 let createTasks = () => {
-  tasks.innerHTML += `
-  <div>
-        <span class="fw-bold">${data.text}</span>
-        <span class="small text-secondary">${data.date}</span>
-        <p>${data.description}</p>
-
-        <span class="options">
-          <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
-          <i onClick ="deleteTask(this)" class="fas fa-trash-alt"></i>
-        </span>
-      </div>
-  `;
+  tasks.innerHTML = "";
+  data.map((x, y) => {
+    return (tasks.innerHTML += `
+    <div id=${y}>
+          <span class="fw-bold">${x.text}</span>
+          <span class="small text-secondary">${x.date}</span>
+          <p>${x.description}</p>
+  
+          <span class="options">
+            <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
+            <i onClick ="deleteTask(this)" class="fas fa-trash-alt"></i>
+          </span>
+        </div>
+    `);
+  });
 
   resetForm();
 };
 
 let deleteTask = (e) => {
   e.parentElement.parentElement.remove();
+  data.splice(e.parentElement.parentElement.id, 1);
+  localStorage.setItem("data", JSON.stringify(data));
+
+  console.log(data);
 };
 
 let editTask = (e) => {
@@ -66,7 +77,7 @@ let editTask = (e) => {
   dateInput.value = selectedTask.children[1].innerHTML;
   textarea.value = selectedTask.children[2].innerHTML;
 
-  selectedTask.remove();
+  deleteTask(e);
 };
 
 let resetForm = () => {
@@ -74,3 +85,9 @@ let resetForm = () => {
   dateInput.value = "";
   textarea.value = "";
 };
+
+(() => {
+  data = JSON.parse(localStorage.getItem("data"));
+  console.log(data);
+  createTasks();
+})();
